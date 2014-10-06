6793d70b6c14ea9572649a87ec45876ab9a88140
Eugene Fridman <pm771.am@gmail.com>
2014-10-05 22:07:07 -0400

    With working timer

--- src/SimpleSS.js
+++ src/SimpleSS.js
@@ -13,39 +13,67 @@ var images = [
                "Penguins.jpg",
                "Tulips.jpg"
              ]
+var delay = 3000 // 3 sec
 
-//window.onoload = function() {
+window.onload = function() {
 	
 	var imageElement = document.getElementById("image_box");
 	var captionElement = document.getElementById("caption");
 	
 	var imageNo = Math.floor(Math.random()*images.length);
-	displayImage();
-	
+	var timeHandler = null;
+	var auto; 
 	document.getElementById("left").addEventListener("click", moveLeft);
-	document.getElementById("right").addEventListener("click", moveRight);
+	document.getElementById("right").addEventListener("click", manualRight);
+	
+	manualRight();
 	
 	function moveLeft() {
 		// decrement image no
 		 imageNo = (imageNo - 1 + images.length) % images.length;
-		
+		 
 		displayImage();
+		auto = false;
+		resetTimer();
 	}
 	
 	function moveRight() {
 		// increment image no
 		imageNo = (imageNo + 1) % images.length;
-	
+			
 		displayImage();
+		
+		if( ! auto ) {
+			resetTimer();
+			auto = true;
+		}
 	}
 	
 	function displayImage() {
 		var imageFile = images[imageNo];
-		var imageName = imageFile.split(".")[0];
+		var imageName = imageFile.split(".")[0];						
 		
 		imageElement.src = "images/" + imageFile;
 		imageElement.alt = imageName;
 		imageElement.title=imageFile;
 		captionElement.innerHTML = imageName;
 	}
-//}
+	
+	function resetTimer() {
+		if(timeHandler) {
+			window.clearInterval(timeHandler);
+		}
+		timeHandler = window.setInterval(autoRight, delay);
+	}
+	
+	function manualRight() {
+		auto = false;
+		moveRight();
+		
+	}
+	
+	function autoRight() {
+		auto = true;
+		moveRight();
+	}
+}
